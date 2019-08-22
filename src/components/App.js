a5d5c6f427052ae6ebdb566a2ecd8c24e2555fa8
Rafael Teixeira <fael.teixeira@gmail.com>
2019-08-22 02:51:18 -0300

    Criado um contador simples

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import Contador from './Contador';
+
 export default class App extends React.Component{
 	render(){
-		return (<h1>Olá React</h1>);
+		return (
+			<Contador/>
+		);
 	}
 }
